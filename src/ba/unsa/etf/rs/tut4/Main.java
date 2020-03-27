9404eccb96af11f3e6673b0109d1b0dc21bc8603
zlicina1 <zlicina1@etf.unsa.ba>
2020-03-27 22:43:19 +0100

    Dodan kor. interfejs u main.fxml( tj. "Artikli")

--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -1,7 +1,23 @@
 package ba.unsa.etf.rs.tut4;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Sretan rad na tutorijalu 4!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        stage.setTitle("Kasa");
+        stage.setScene(new Scene(root));
+
+        stage.show();
     }
 }
---
+++ src/ba/unsa/etf/rs/tut4/Racun.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rs.tut4;
+
+public class Racun {
+    private double kolicina;
+
+
+    public void dodajStavku(Artikal artikal, int i) {
+    }
+
+    public double ukupanIznos() {
+        double suma;
+        return suma = 0;
+    }
+}
